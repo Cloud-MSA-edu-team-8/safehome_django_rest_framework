25236d39de7f19a1f1e0a9e17bf47244c74091e6
dev-yujin <kyouj96@gmail.com>
2020-08-29 03:19:58 +0900

    datadump add

--- mytest/settings.py
+++ mytest/settings.py
@@ -22,6 +22,8 @@ import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# 수정
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 dotenv_file = os.path.join(BASE_DIR, ".env")
 if os.path.isfile(dotenv_file):
     dotenv.load_dotenv(dotenv_file)
