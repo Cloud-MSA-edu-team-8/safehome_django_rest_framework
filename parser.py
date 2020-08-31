d9bb2296ab9ca0f88802589bc67329c2f19ffdc4
dev-yujin <kyouj96@gmail.com>
2020-08-31 18:04:39 +0900

    change scheduler time, add keyword

--- parser.py
+++ parser.py
@@ -14,19 +14,19 @@ django.setup()
 from newscrawl.models import NewsData
 
 
-def parse_news():
-    search_keyword = '서울시 안전'
-    base = f'https://search.naver.com/search.naver?where=news&sm=tab_jum&query={search_keyword}'
+def parse_news(keyword):
+    data = {}
+    search_keyword = keyword
+
+    # for idx in [0,11]: # 여러 페이지를 가지고 올때 사용합니다.
+    base = f'https://search.naver.com/search.naver?where=news&sm=tab_jum&query={search_keyword}&start={0}'
 
     req= requests.get(base)
-    html = req.text
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(req.text, 'html.parser')
     my_title = soup.select('a._sp_each_title') # 제목, 링크
     my_contents = soup.select('ul.type01 dl > dd:nth-child(3)') # 내용
 
-    data = {}
-
-    for idx, i in enumerate(my_title[:5]):
+    for idx, i in enumerate(my_title): #10개씩
         title = i['title']
         link = urljoin(base, i['href'])
         contents = my_contents[idx].text
@@ -36,17 +36,29 @@ def parse_news():
     return data
 
 
-def del_save_data():
-    NewsData.objects.all().delete() # 객체 전체 삭제
-    news_data_dict = parse_news() # 현재시간 뉴스 받아옴
+def del_save_data(keyword):
+    ban_list = ['코로나', '마스크', '교회', '방역', '확진', '공모전', '인공지능']
+
+    news_data_dict = parse_news(keyword) # 현재시간 뉴스 받아옴
+    
+    # print(news_data_dict)
     print(len(news_data_dict))
 
-    for t, l in news_data_dict.items(): # 객체 저장
-        NewsData(title=t, link=l[0], contents=l[1]).save()
+    cnt = 0
+    for t, l in news_data_dict.items():
+        if not any([ban in t or ban in l[1] for ban in ban_list]):
+            print(t)
+            NewsData(title=t, link=l[0], contents=l[1]).save() # 저장
+            cnt += 1
+        if cnt == 2: # 검색어 하나당 기사 두개
+            break
 
 def main():
-    del_save_data()
-
-
+    NewsData.objects.all().delete() # 객체 전체 삭제
+    del_save_data('서울집값')
+    del_save_data('서울시안전')
+    del_save_data('서울살인')
+    del_save_data('서울시홍수피해지역')
+    
 if __name__ == '__main__':
     main()
--- parser_scheduler.py
+++ parser_scheduler.py
@@ -7,14 +7,10 @@ import parser as pr
 sched = BlockingScheduler()
 local_tz = pytz.timezone('Asia/Seoul')
 
-@sched.scheduled_job('interval', minutes=1)
+@sched.scheduled_job('interval', minutes=60*6) # 6시간
 def timed_job():
     print('This job is run every one minutes.')
 
     pr.del_save_data() # news update
-
-# @sched.scheduled_job('cron', day_of_week='mon-fri', hour=17)
-# def scheduled_job():
-#     print('This job is run every weekday at 5pm.')
 
 sched.start()
